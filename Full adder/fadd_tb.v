506d32237d7031b68de36bca3c13a52172271c19
Rueicinglin <141209276+Rueicinglin@users.noreply.github.com>
2023-08-04 14:24:01 +0800

    Update fadd_tb.v

--- Full adder/fadd_tb.v
+++ Full adder/fadd_tb.v
@@ -2,37 +2,37 @@
 
 module fadd_tb;
 
-    reg a, b, cin;
-	 wire sum, cout;
+reg a, b, cin;
+wire sum, cout;
 	 
-	 fadd FA1 (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+fadd FA1 (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 	 
-	 initial
-	 begin
-	     a = 1'b0;
-		  b = 1'b0;
-		  cin = 1'b0;
-		  #5 cin = 1'b1;
-		  #5;
-		  b = 1'b1;
-		  cin = 1'b0;
-		  #5 cin = 1'b1;
-		  #5;
-		  a = 1'b1;
-		  b = 1'b0;
-		  cin = 1'b0;
-		  #5 cin = 1'b1;
-		  #5;
-		  b = 1'b1;
-		  cin = 1'b0;
-		  #5 cin = 1'b1;
-		  #5 $stop;
-		  #5 $finish;
-	 end
+initial
+begin
+a = 1'b0;
+b = 1'b0;
+cin = 1'b0;
+#5 cin = 1'b1;
+#5;
+b = 1'b1;
+cin = 1'b0;
+#5 cin = 1'b1;
+#5;
+a = 1'b1;
+b = 1'b0;
+cin = 1'b0;
+#5 cin = 1'b1;
+#5;
+b = 1'b1;
+cin = 1'b0;
+#5 cin = 1'b1;
+#5 $stop;
+#5 $finish;
+end
 	 
-	 initial
-	 begin
-	    $monitor($time, "a=%b b=%b cin=%b sum=%b cout=%b", a, b, cin, sum, cout);
-	 end
+initial
+begin
+$monitor($time, "a=%b b=%b cin=%b sum=%b cout=%b", a, b, cin, sum, cout);
+end
 	 
 endmodule
