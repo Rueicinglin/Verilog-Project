eff3d90fe74473bfa3d5c8bedcc9472ab6497fd7
Rueicinglin <141209276+Rueicinglin@users.noreply.github.com>
2023-08-04 11:24:48 +0800

    Update div3_tb1.v

--- Div3/div3_tb1.v
+++ Div3/div3_tb1.v
@@ -2,24 +2,24 @@
 
 module div3_tb1;
 
-    reg clk, rst_n;
-	 wire div3_out;
+reg clk, rst_n;
+wire div3_out;
 	 
-	 div3 D1 (.clk(clk), .rst_n(rst_n), .div3_out(div3_out));
+div3 D1 (.clk(clk), .rst_n(rst_n), .div3_out(div3_out));
 	 
-	 initial
-	 begin
-	     clk = 1'b0;
-		  rst_n = 1'b1;
-		  forever #5 clk = ~clk;
-	 end
+initial
+begin
+clk = 1'b0;
+rst_n = 1'b1;
+forever #5 clk = ~clk;
+end
 	 
-	 initial
-	 begin
-	     #10 rst_n = 1'b0;
-		  #10 rst_n = 1'b1;
-		  #880 $stop;
-		  #10 $finish;
-	 end
+initial
+begin
+#10 rst_n = 1'b0;
+#10 rst_n = 1'b1;
+#880 $stop;
+#10 $finish;
+end
 	 
 endmodule
