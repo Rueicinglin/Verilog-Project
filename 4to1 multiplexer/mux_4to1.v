566658dba9502dd15ba190e2df192bb986cf0ddb
Rueicinglin <141209276+Rueicinglin@users.noreply.github.com>
2023-08-04 11:15:50 +0800

    Update mux_4to1.v

--- 4to1 multiplexer/mux_4to1.v
+++ 4to1 multiplexer/mux_4to1.v
@@ -1,17 +1,17 @@
 module mux_4to1 (
-                 input [3:0] a, b, c, d,
-					  input [1:0] sel,
-					  output reg [3:0] out
-					  );
+input [3:0] a, b, c, d,
+input [1:0] sel,
+output reg [3:0] out
+);
 					  
-		always @(a, b, c, d, sel)
-		begin
-		     case (sel)
-			     2'b00: out = a;
-				  2'b01: out = b;
-				  2'b10: out = c;
-				  2'b11: out = d;
-			  endcase
-	   end
+always @(a, b, c, d, sel)
+begin
+case (sel)
+2'b00: out = a;
+2'b01: out = b;
+2'b10: out = c;
+2'b11: out = d;
+endcase
+end
 		
 endmodule
